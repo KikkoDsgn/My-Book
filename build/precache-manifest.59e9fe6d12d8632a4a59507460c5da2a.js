253334e2def2a542f70bf2f2f01a9d0f2402c6e5
KikkoDsgn <66521828+KikkoDsgn@users.noreply.github.com>
2020-07-06 23:18:26 +0200

    Add files via upload

---
+++ build/precache-manifest.59e9fe6d12d8632a4a59507460c5da2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ef33dfcae52c7d793fff2896085193",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6eb6d30c39039a1d027",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "89d96297f9398e20a724",
+    "url": "/static/css/main.16da0e1a.chunk.css"
+  },
+  {
+    "revision": "b6eb6d30c39039a1d027",
+    "url": "/static/js/2.3d5ddb83.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.3d5ddb83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89d96297f9398e20a724",
+    "url": "/static/js/main.76dd783b.chunk.js"
+  },
+  {
+    "revision": "9546fa9ca4594cbfa408",
+    "url": "/static/js/runtime-main.cd4efa04.js"
+  }
+]);
